b274e5ff055e3a57f17a0511189fc9133e7d4827
Daniel Weber <danielcweber@users.noreply.github.com>
2024-03-03 17:24:47 +0100

    Don't serve files. Only ever redirect.

--- main.c
+++ main.c
@@ -3,59 +3,20 @@
 
 #define CHUNK_SIZE 1024 // read 1024 bytes at a time
 
-// Public directory settings
-#define PUBLIC_DIR "./public"
-#define NOT_FOUND_HTML "/404.html"
-
 int main(int c, char **v) {
   char *port = c == 1 ? "8000" : v[1];
   serve_forever(port);
   return 0;
 }
 
-int file_exists(const char *file_name) {
-  struct stat buffer;
-  int exists;
-
-  exists = (stat(file_name, &buffer) == 0);
-
-  return exists;
-}
-
-int read_file(const char *file_name) {
-  char buf[CHUNK_SIZE];
-  FILE *file;
-  size_t nread;
-  int err = 1;
-
-  file = fopen(file_name, "r");
-
-  if (file) {
-    while ((nread = fread(buf, 1, sizeof buf, file)) > 0)
-      fwrite(buf, 1, nread, stdout);
-
-    err = ferror(file);
-    fclose(file);
-  }
-  return err;
-}
-
 void route() {
   ROUTE_START()
 
   GET(uri) {
-    char file_name[255];
-    snprintf(file_name, sizeof(file_name), "%s%s", PUBLIC_DIR, uri);
 
-    if (file_exists(file_name)) {
-      HTTP_200;
-      read_file(file_name);
-    } else {
-      HTTP_404;
-      sprintf(file_name, "%s%s", PUBLIC_DIR, NOT_FOUND_HTML);
-      if (file_exists(file_name))
-        read_file(file_name);
-    }
+    printf("%s 302 OK\n", RESPONSE_PROTOCOL);
+    printf("Content-Length: 0\n");
+    printf("Location: https://github.com%s\n\n", uri);
   }
 
   ROUTE_END()
