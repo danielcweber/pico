8b050f321b7f26b170b42d86b4df1a8c6c77acdb
Daniel Weber <danielcweber@users.noreply.github.com>
2024-03-03 17:36:03 +0100

    Use prot in response.

--- httpd.h
+++ httpd.h
@@ -31,12 +31,12 @@ void route();
 // Response
 #define RESPONSE_PROTOCOL "HTTP/1.1"
 
-#define HTTP_200 printf("%s 200 OK\n\n", RESPONSE_PROTOCOL)
-#define HTTP_201 printf("%s 201 Created\n\n", RESPONSE_PROTOCOL)
-#define HTTP_302 printf("%s 302 Found\n\n", RESPONSE_PROTOCOL)
-#define HTTP_404 printf("%s 404 Not found\n\n", RESPONSE_PROTOCOL)
-#define HTTP_500 printf("%s 500 Internal Server Error\n\n", RESPONSE_PROTOCOL)
-#define HTTP_403 printf("%s 403 Forbidden\n\n", RESPONSE_PROTOCOL)
+#define HTTP_200 printf("%s 200 OK\n\n", prot)
+#define HTTP_201 printf("%s 201 Created\n\n", prot)
+#define HTTP_302 printf("%s 302 Found\n", prot)
+#define HTTP_404 printf("%s 404 Not found\n\n", prot)
+#define HTTP_500 printf("%s 500 Internal Server Error\n\n", prot)
+#define HTTP_403 printf("%s 403 Forbidden\n\n", prot)
 
 // some interesting macro for `route()`
 #define ROUTE_START() if (0) {
